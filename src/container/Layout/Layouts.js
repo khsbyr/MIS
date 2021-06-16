17c7e9fae006c0abb884b035e112aa2486104645
Aitmukhamyed Almasbek <aitmukhamyed@dlp.mn>
2021-06-16 10:56:13 +0800

    component

--- src/container/Layout/Layouts.js
+++ src/container/Layout/Layouts.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { withRouter } from "react-router-dom";
 import { Layout as LayoutWrapper } from "antd";
 import Headers from "./Header/Header";
+import Pageheader from "./component/Pageheader";
 
 
 const { Content } = LayoutWrapper;
@@ -10,6 +11,10 @@ export default withRouter(function Layout({ children }) {
   return (
     <Fragment>
       <Headers />
+      {/* <Pageheader /> */}
+      {/* {routes.map(({ path, component, exact = false }) => (
+            <Route key={path} path={path} exact={exact} component={component} />
+          ))} */}
       <Content>{children}</Content>
     </Fragment>
   );
