61e0bc79203bdefeee12382abff8bcf7dd99a9a4
khosbayar <khosbayar.b@dlp.mn>
2021-07-02 17:44:00 +0800

    table changed

--- src/pages/criteria/criteria.js
+++ src/pages/criteria/criteria.js
@@ -1,12 +1,14 @@
 import {
-    ExclamationCircleOutlined, FileOutlined, FileSyncOutlined, FolderAddFilled, PrinterOutlined, SettingFilled
+    ExclamationCircleOutlined, FileOutlined, FileSyncOutlined, DownOutlined, PrinterOutlined, CopyOutlined, SearchOutlined
 } from "@ant-design/icons";
 import SaveIcon from "@material-ui/icons/Save";
-import { Button, Col, Dropdown, Form, Layout, Menu, message, Modal, Row, DatePicker, Tabs } from "antd";
+import { Button, Col, Dropdown, Form, Layout, Menu, message, Modal, Row, DatePicker, Input } from "antd";
 import { Column } from "primereact/column";
 import { DataTable } from "primereact/datatable";
 import Loader from "../../loader/Loader";
 import React, { useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import { isShowLoading } from "../../context/Tools";
 import { getService, putService } from "../../service/service";
 import { PAGESIZE } from "../../tools/Constant";
@@ -14,18 +16,20 @@ import { errorCatch, convertLazyParamsToObj } from "../../tools/Tools";
 import CriteriaModal from "../criteria/components/CriteriaModal";
 import "./criteria.style"
 import ContentWrapper from "./criteria.style";
+import { faFileExcel, faPen, faPlus, faPrint, faTrash } from "@fortawesome/free-solid-svg-icons";
 function handleMenuClick(e) { console.log("click", e.key[0]); }
 function onChange(date, dateString) {
     console.log(date, dateString);
 }
 const { Content } = Layout;
+const onSearch = value => console.log(value);
+
 const menu = (
     <Menu onClick={handleMenuClick}>
         <Menu.Item
             key="1"
-            icon={<FileSyncOutlined style={{ fontSize: "14px", color: "#45629c" }} />}
+            icon={ <FontAwesomeIcon icon={faPrint} />}
         >
-
             Импорт
         </Menu.Item>
         <Menu.Item
@@ -65,6 +69,15 @@ const Criteria = () => {
         onInit();
     }, [lazyParams])
 
+    // const actionBodyTemplate = (row) => {
+    //     return (
+    //         <React.Fragment>
+    //             <Button type="text" icon={ <FontAwesomeIcon icon={faPen}/>} onClick={edit} />
+    //             <Button type="text" icon={ <FontAwesomeIcon icon={faTrash}/>} onClick={pop} />
+    //         </React.Fragment>
+    //     );
+    // }
+
     const onInit = () => {
         setLoading(true);
         if (loadLazyTimeout) {
@@ -91,7 +104,17 @@ const Criteria = () => {
         setIsModalVisible(true);
         isEditMode = false;
     };
-       const edit = (row) => {
+
+    const action = () => {
+        return (   
+            <React.Fragment>
+                <Button type="text" icon={ <FontAwesomeIcon icon={faPen}/>} onClick={edit} />
+                <Button type="text" icon={ <FontAwesomeIcon icon={faTrash}/>} onClick={pop} />
+            </React.Fragment>
+        );
+    }
+
+    const edit = (row) => {
         editRow = row.data
         isEditMode = true
         setIsModalVisible(true)
@@ -124,45 +147,62 @@ const Criteria = () => {
             confirm();
         }
     };
-    const [selectedProducts, setSelectedProducts] = useState(null);
     return (
         <ContentWrapper>
             <div className="button-demo">
-                <Layout className="btn-layout">
+            <Layout className="btn-layout">
                     <Content>
                         <Row>
-                            <Col>
-                                <h2 className="title">Шалгуур үзүүлэлт</h2>
+                            <Col xs={24} md={24} lg={14}>
+                                <p className="title">Сургалтын хөтөлбөр</p>
+                            </Col>
+                            <Col xs={24} md={24} lg={10}>
+                                <Row>
+                                    <Col xs={8} md={8} lg={6}>
+                                        <DatePicker
+                                            onChange={onChange}
+                                            bordered={false}
+                                            suffixIcon={<DownOutlined />}
+                                            placeholder="Select year"
+                                            picker="year"
+                                            className="DatePicker"
+                                            style={{
+                                                width: "120px",
+                                                color: "black",
+                                                cursor: "pointer",
+                                            }}
+                                        />
+                                    </Col>
+                                    {/* <Col xs={8} md={8} lg={6}>
+                                        <Input
+                                            placeholder="Хайлт хийх"
+                                            allowClear
+                                            prefix={<SearchOutlined />}
+                                            bordered={false}
+                                            onSearch={onSearch}
+                                            style={{
+                                                width: 150,
+                                                borderBottom: "1px solid #103154",
+                                            }}
+                                        />
+                                    </Col> */}
+                                    <Col xs={8} md={8} lg={6}>
+                                        <Button type="text" icon={ <FontAwesomeIcon icon={faPrint}/>} >Хэвлэх </Button>
+                                    </Col>
+                                    <Col xs={8} md={8} lg={6}>
+                                        <Button type="text" className="export" icon={ <FontAwesomeIcon icon={faFileExcel}/>} >
+                                            Экспорт
+                                        </Button>
+                                    </Col>
+                                    <Col xs={8} md={8} lg={6}>
+                                        <Button type="text" className="export" icon={ <FontAwesomeIcon icon={faPlus}/>} onClick={add}>
+                                            Нэмэх
+                                        </Button>
+                                    </Col>
+                                </Row>
                             </Col>
                         </Row>
-                        <Row>
-                            <Col span={2}>
-                                <Button onClick={add} type="link" icon={<SaveIcon />}>
-                                    Нэмэх
-                                </Button>
-                            </Col>
-                            <Col span={2}>
-                                <Button onClick={pop} type="link" icon={<FolderAddFilled />}>
-                                    Устгах
-                                </Button>
-                            </Col>
-                            <Col span={2}>
-                                <DatePicker onChange={onChange} picker="year" />
-                            </Col>
-                            <Col span={18} style={{ textAlign: "right" }}>
-                                <div style={{ marginRight: "5px" }}>
-                                    <Dropdown.Button
-                                        overlay={menu}
-                                        placement="bottomCenter"
-                                        icon={
-                                            <SettingFilled
-                                                style={{ marginLeft: "8px", color: "#45629c" }}
-                                            />
-                                        }
-                                    ></Dropdown.Button>
-                                </div>
-                            </Col>
-                        </Row>
+
                     </Content>
                 </Layout>
                 <div className="datatable-responsive-demo">
@@ -172,24 +212,26 @@ const Criteria = () => {
                         paginator 
                         rows={10}
                         className="p-datatable-responsive-demo"
-                        selectionMode="checkbox"
+                        // selectionMode="checkbox"
                         selection={selectedRows}
-                        onRowClick={edit}
+                        // onRowClick={edit}
+                        // editMode="row"
                         onSelectionChange={(e) => {
                             setSelectedRows(e.value);
                         }}
                         dataKey="id">
-                        <Column selectionMode="multiple" headerStyle={{ width: '3em', padding: "0px" }}  ></Column>
-                        <Column field="index" header="№" sortable />
-                        <Column field="code" header="Код" sortable />
-                        <Column field="name" header="Шалгуур үзүүлэлтийн нэр" style={{ textAlign: "left" }} sortable filter filterPlaceholder="Хайх" />
-                        <Column field="indicatorProcess" header="Хүрэх үр дүн" sortable filter filterPlaceholder="Хайх" />
-                        <Column field="upIndicator" header="Үр дүнгийн биелэлт" sortable />
-                        <Column field="" header="Шалгуур үзүүлэлтийн төрөл" sortable />
+                        {/* <Column selectionMode="multiple" headerStyle={{ width: '3em', padding: "0px" }}  ></Column> */}
+                        <Column field="index" header="№" sortable headerStyle={{ width: '5rem' }} bodyStyle={{ textAlign: 'center' }}/>
+                        <Column field="code" header="Код" sortable headerStyle={{ width: '5rem' }} bodyStyle={{ textAlign: 'center' }}/>
+                        <Column field="name" header="Шалгуур үзүүлэлтийн нэр" headerStyle={{ width: '40rem' }}  style={{ textAlign: "left" }} sortable filter filterPlaceholder="Хайх" />
+                        <Column field="indicatorProcess" header="Хүрэх үр дүн" sortable headerStyle={{ width: '9rem' }} bodyStyle={{ textAlign: 'center' }} filter filterPlaceholder="Хайх" />
+                        <Column field="upIndicator" header="Үр дүнгийн биелэлт" sortable headerStyle={{ width: '11rem' }} bodyStyle={{ textAlign: 'center' }}/>
+                        <Column field="" header="Шалгуур үзүүлэлтийн төрөл" sortable headerStyle={{ width: '14rem' }} bodyStyle={{ textAlign: 'center' }}/>
+                        <Column headerStyle={{ width: '7rem' }} body={action}></Column>
                     </DataTable>
                     {isModalVisible && (
                         <CriteriaModal
-                        Criteriacontroller={editRow}
+                            Criteriacontroller={editRow}
                             isModalVisible={isModalVisible}
                             close={closeModal}
                             isEditMode={isEditMode}
