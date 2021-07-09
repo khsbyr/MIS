5b21c9fc503d1ce42fa6bdbdbf9afaa8e86ce271
Erdenebileg <erdenebileg@dlp.mn>
2021-07-09 15:54:30 +0800

    Fix

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,60 +1,19 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-import Loadable from "react-loadable";
-import { ADMIN_PAGE, LOGIN_PAGE, REGISTER } from "../settings/constantRoutes";
+import { ADMIN_PAGE, REGISTER } from "../settings/constantRoutes";
 import Layouts from "../container/Layout/Layouts";
-
-/**
- *
- * Public Routes
- *
- */
-
-const Loading = () => null;
-const routes = [
-  {
-    path: LOGIN_PAGE,
-    component: Loadable({
-      loader: () =>
-        import(/* webpackChunkName: "Home" */ "../pages/auth/login"),
-      loading: Loading,
-      modules: ["Home"],
-    }),
-    exact: true,
-  },
-  {
-    path: REGISTER,
-    component: Loadable({
-      loader: () =>
-        import(/* webpackChunkName: "Home" */ "../pages/auth/Register"),
-      loading: Loading,
-      modules: ["Register"],
-    }),
-  },
-  {
-    path: ADMIN_PAGE,
-    component: Loadable({
-      loader: () =>
-        import(/* webpackChunkName: "Home" */ "../pages/admin"),
-      loading: Loading,
-      modules: ["admin"],
-    }),
-  },
-];
-/**
- *
- * Overall Router Component
- *
- */
+import Login from "../pages/auth/login";
+import Admin from "../pages/admin";
+import Register from "../pages/auth/Register";
 
 class Routes extends Component {
   render() {
     return (
         <Layouts>
         <Switch>
-          {routes.map(({ path, component, exact = false }) => (
-            <Route key={path} path={path} exact={exact} component={component} />
-          ))}
+          <Route path={REGISTER} component={Register} />
+          <Route path={ADMIN_PAGE} component={Admin} />
+          <Route path="/" component={Login} />
         </Switch>
         </Layouts>
     );
--- src/service/axios.js
+++ src/service/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    //baseURL: process.env.REACT_APP_API_URL
-    baseURL: "http://192.168.10.18:3000/"
+    baseURL: process.env.REACT_APP_API_URL
+    //baseURL: "http://192.168.10.18:3000/"
 })
