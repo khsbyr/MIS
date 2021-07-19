9a8730a7efb22da82fa0926d6ae07df04f059649
Erdenebileg <erdenebileg@dlp.mn>
2021-07-19 15:30:48 +0800

    passwordReset

--- src/service/service.js
+++ src/service/service.js
@@ -7,6 +7,7 @@ const config = () => ({
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "*",
+    "AccessToken": localStorage.getItem('token'),
   },
 });
 
