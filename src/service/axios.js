5609d48ba4ae5a5d20de79a375a9e50ef0ec7772
khosbayar <khosbayar.b@dlp.mn>
2021-07-23 14:39:23 +0800

    axios

--- src/service/axios.js
+++ src/service/axios.js
@@ -2,10 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
     //baseURL: process.env.REACT_APP_API_URL
-<<<<<<< HEAD
-    baseURL: "http://192.168.10.18:3000"
-=======
     baseURL: "http://192.168.10.18:3000/"
->>>>>>> e966fc91878f20505fcd7b9d110d085363e31824
     // baseURL: "http://47.243.61.40:3000"
 })
