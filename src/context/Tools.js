f42a299f0cdb9c10fed0886451e03c0d9fc20019
Erdenebileg <erdenebileg@dlp.mn>
2021-07-26 13:13:58 +0800

    Fix

--- src/context/Tools.js
+++ src/context/Tools.js
@@ -1,31 +1,13 @@
-/* eslint-disable import/no-anonymous-default-export */
 import React, { useState } from 'react';
 import Loader from '../loader/Loader';
 
 export const ToolsContext = React.createContext();
 
-// var ref = null
-
-// export function toolsStore.setIsShowLoader(utga) {
-//     if (!!ref) {
-//         ref.current.toolsStore.setIsShowLoader(utga);
-//     }
-// }
-
 export default function ContextProvider({ children }) {
-  // ref = useRef(null)
   const [isShowLoader, setIsShowLoader] = useState(false);
   const [tokenParsed, setTokenParsed] = useState();
   const [user, setUser] = useState();
   const [org, setOrg] = useState();
-
-  // React.useImperativeHandle(
-  //     ref,
-  //     () => ({
-  //         toolsStore.setIsShowLoader: (utga) => { setIsShowLoader(utga) }
-  //     }),
-  //     [],
-  // )
 
   return (
     <ToolsContext.Provider
