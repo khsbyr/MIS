23814a233e96a906ec7377332b734366773b8be9
Erdenebileg Chinchuluun <ebi@Erdenebilegs-MacBook-Pro.local>
2021-08-12 15:22:21 +0800

    fixAll

---
+++ src/pages/project/project.style.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const ContentWrapper = styled.div`
+  margin-left: 35px;
+  margin-right: 35px;
+  margin-top: 30px;
+
+  .title {
+    font-size: 24px;
+    font-weight: bold;
+    color: #103154;
+  }
+
+  Select {
+    text-align: center;
+  }
+
+  .ant-select-single:not(.ant-select-customize-input) .ant-select-selector {
+    height: 33px;
+    padding-top: 6px;
+    border: none;
+  }
+`;
+
+export default ContentWrapper;
