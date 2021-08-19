41bb3afad17cbd89a0ea4c9124b2351a4a78a31b
Erdenebileg Chinchuluun <ebi@Erdenebilegs-MacBook-Pro.local>
2021-08-19 11:24:33 +0800

    fixAll

--- src/service/axios.js
+++ src/service/axios.js
@@ -3,5 +3,6 @@ import axios from 'axios';
 export default axios.create({
   // baseURL: process.env.REACT_APP_API_URL
   // baseURL: 'http://192.168.20.222:3000',
-  baseURL: 'http://47.243.61.40:3000',
+  // baseURL: 'http://47.243.61.40:3000',
+  baseURL: 'http://localhost:8000',
 });
